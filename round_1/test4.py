e260f4e2349eeea4d2a59056aefa533055549e8e
cungh-kit <ltv.h.cung001@gmail.com>
2026-04-21 16:28:49 +0700

    Done Part 1, Q1-Q10

--- round_1/test4.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('../dataset/Operational/web_traffic.csv')
-mean_bounce_rate = df.groupby('traffic_source')['bounce_rate'].mean().sort_values()
-print(mean_bounce_rate)
